e9009c3c8198a8b035850396f18e9c93f0495c03
岡本悠希 <okamotoyuuki0615@gmail.com>
2018-11-09 19:02:05 +0900

    操作キャラを変更するスクリプトの追加

---
+++ Necro/Assets/Scripts/okamoto/PlayerData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData
+{
+    public readonly static PlayerData Instance = new PlayerData();
+
+    public int m_HP;
+    public int m_zombieNum;
+
+}
