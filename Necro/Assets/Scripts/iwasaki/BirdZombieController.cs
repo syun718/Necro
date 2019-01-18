1380f3ae12529df6ee70f058007e4a85b73dbb4c
岩崎駿 <25253535.syun718@gmail.com>
2019-01-18 19:14:47 +0900

    犬ゾンビ撮影準備完了

--- Necro/Assets/Scripts/iwasaki/BirdZombieController.cs
+++ Necro/Assets/Scripts/iwasaki/BirdZombieController.cs
@@ -24,19 +24,41 @@ public class BirdZombieController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector2(-Parameter_Speed * 0.05f, rb.velocity.y));
+        if (Input.GetKeyDown("a")) //プレイヤーがダッシュ圏内か
+        {
+            Debug.Log("ジャンプ");
+            rb.AddForce(Vector2.up * 300.0f);
+        }
+
+        if (Input.GetKeyDown("d")) //プレイヤーがダッシュ圏内か
+        {
+            Debug.Log("滑空");
+            rb.AddForce(Vector2.left * 200.0f);
+            rb.AddForce(Vector2.down * 100.0f);
+        }
+
+        //transform.Translate(new Vector2(-Parameter_Speed * 0.05f, rb.velocity.y));
         if (LaterChance_count == 0)
         {
+            transform.Translate(new Vector2(-Parameter_Speed * 0.05f, rb.velocity.y));
             State = 0;
 
-            if ((new System.Random().Next(200) == 0) || (transform.position.x >= 7 || transform.position.x <= -7))
+            //if ((new System.Random().Next(200) == 0) || (transform.position.x >= 7 || transform.position.x <= -7))
+            //{
+            //    Debug.Log("向き転換");
+            //    State = 1;
+            //}
+            if (Input.GetKeyDown("a")) //プレイヤーがダッシュ圏内か
             {
-                Debug.Log("向き転換");
-                State = 1;
+                Debug.Log("ジャンプ");
+                rb.AddForce(Vector2.up * 50.0f);
             }
             else if (Input.GetKeyDown("d")) //プレイヤーがダッシュ圏内か
             {
-                rb.AddForce(Vector2.up * 40.0f);
+                rb.AddForce(Vector2.up * 50.0f);
+
+
+
                 if (new System.Random().Next(50) == 0)
                 {
                     Debug.Log("ダッシュ開始");
@@ -44,21 +66,21 @@ public class BirdZombieController : MonoBehaviour
                     State = 2;
                 }
             }
-        }
-        else
-        {
-            switch (State)
+            else
             {
-                case 1:
-                    Attack();
-                    break;
+                switch (State)
+                {
+                    case 1:
+                        Attack();
+                        break;
 
-                case 2:
-                    Dash();
-                    break;
+                    case 2:
+                        Dash();
+                        break;
+                }
+
+                LaterChance_count--;
             }
-
-            LaterChance_count--;
         }
     }
 
