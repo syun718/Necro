cc6b80b93a7cae2c9783204a7c27db78bc87e673
神里 <takoyaki_4423@ezweb.ne.jp>
2019-02-09 13:52:10 +0900

    急造のTitleとStage

---
+++ Necro/Assets/Scripts/kamisato/Scene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour {
+
+    PlayerInput m_playerInput;
+
+    // Use this for initialization
+    void Start () {
+        m_playerInput = GetComponent<PlayerInput>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ButtonClicked();
+    }
+
+    public void ButtonClicked()
+    {
+        Debug.Log("aaa");
+        if (Input.GetKey("left"))
+        {
+            Debug.Log("aaa");
+            SceneManager.LoadScene("Stage1");
+        }
+        
+    }
+}
