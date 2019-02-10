67987f1375d50348f9589d7e8648f0589eddfbb1
神里 <takoyaki_4423@ezweb.ne.jp>
2019-02-11 04:44:03 +0900

    up

--- Necro/Assets/Scripts/kamisato/Scene.cs
+++ Necro/Assets/Scripts/kamisato/Scene.cs
@@ -7,15 +7,17 @@ using UnityEngine.SceneManagement;
 public class Scene : MonoBehaviour {
 
     PlayerInput m_playerInput;
-
     // Use this for initialization
     void Start () {
         m_playerInput = GetComponent<PlayerInput>();
     }
 	
+    
+
 	// Update is called once per frame
 	void Update () {
-        ButtonClicked();
+        StartCoroutine("LoadStart");
+        //ButtonClicked();
     }
 
     public void ButtonClicked()
@@ -26,6 +28,6 @@ public class Scene : MonoBehaviour {
             Debug.Log("aaa");
             SceneManager.LoadScene("Stage1");
         }
-        
+
     }
 }
